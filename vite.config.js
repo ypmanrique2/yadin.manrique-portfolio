f2e0cf5440edcafe12a76b979af855a66887ebdc
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2025-03-26 08:13:49 -0500

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
+import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/yadin.manrique-portfolio",
+  base: '/yadin.manrique-portfolio/'
 });
